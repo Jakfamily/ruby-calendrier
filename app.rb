7cd786c3bd9cb23d70e1561153223cb83b7a0722
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-22 15:25:58 +0200

    mise en place de la class User + test de fonctionaliter sur app.rb

---
+++ app.rb
@@ -0,0 +1,7 @@
+require './lib/user'
+require './lib/event'
+
+
+julie = User.new("julie@email.com", 32)
+puts julie.email
+puts julie.age
---
+++ lib/user.rb
@@ -0,0 +1,21 @@
+class User
+ # On crée une variable de classe @@all_users qui sera un array vide
+  @@all_users = []
+
+  # On définit les attributs email et age en lecture et écriture grâce à attr_accessor
+  attr_accessor :email, :age
+
+  # Le constructeur initialize permet de définir les attributs email et age dès la création d'un nouvel utilisateur
+  def initialize(email, age)
+    @email = email
+    @age = age
+
+    # On ajoute le nouvel utilisateur dans l'array @@all_users
+    @@all_users << self # self represente l'instance de la classe 'User' sur laquelle on apelle la methode 
+  end
+
+  # On crée une méthode de classe qui retourne simplement l'array @@all_users
+  def self.all
+    @@all_users
+  end
+end
