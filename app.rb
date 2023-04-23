46d42a0424498e8d1dc7ef11ee25d55072df1c0b
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-23 13:04:07 +0200

    excutions du event.rb

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require './lib/user'
 require './lib/event'
 
 
-julie = User.new("julie@email.com", 32)
-puts julie.email
-puts julie.age
+# julie = User.new("julie@email.com", 32)
+# puts julie.email
+# puts julie.age
+
+
+event = Event.new("2023-04-01 13:00" , 60, "meeting", ["alice", "jhon"])
+puts event.to_s
---
+++ lib/event.rb
@@ -0,0 +1,41 @@
+require 'time'
+
+class Event
+  @@all_events = []
+
+  attr_accessor :start_date, :duration, :title, :attendees
+  #attr_accessor = time, integer, string, array
+
+  def initialize(start_date, duration, title, attendees)
+    @start_date = Time.parse(start_date)# convertie la str en objet time
+    @duration = duration.to_i #convertie en entier
+    @title = title
+    @attendees = attendees
+    @@all_events << self
+  end
+
+  def self.all_events
+    @@all_events
+  end
+
+  def end_date
+    return @start_date + @duration
+  end
+
+  def is_past?
+    @start_date < Time.now
+  end
+
+  def is_future?
+    @start_date > Time.now
+  end
+
+  def is_soon?
+    @start_date - Time.now < 1800 #cimpare la dif entre debut et heure actuelle avec 30min 
+  end
+
+  def to_s
+    "titre : #{@title}, date de debut : #{@start_date}, durée : #{@duration} minutes, invités : #{@attendees}   "
+  end
+
+end
